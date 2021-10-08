5b30ca1b85eb1c8aa6547c33e7e634393f857e8b
kinngh <kinngh@me.com>
2021-10-09 00:04:35 +0530

    🧹 0.1.3 Housekeeping and updates

--- components/subscriptionData.js
+++ components/subscriptionData.js
@@ -1,15 +1,31 @@
+/*
+  If you have multiple subscriptions running at the same time and using forEach on data array, be sure to pass the Key
+  or react will throw a "no HTML was returned" error
+*/
+
 import React from "react";
 
 const SubscriptionData = ({ error, loading, data }) => {
   if (error) return <p>{error.message}</p>;
   if (loading) return <p>Loading</p>;
-  console.log(data);
-  return (
-    <p>
-      Check your console for current active app subscriptions. This is a
-      temporary placeholder page while subscription routing is being tested
-    </p>
-  );
+
+  if (data) {
+    const { id, name, status } =
+      data.currentAppInstallation.activeSubscriptions[0];
+    return (
+      <React.Fragment>
+        <p>
+          <strong>Id:</strong> {id}
+        </p>
+        <p>
+          <strong>Name:</strong> {name}
+        </p>
+        <p>
+          <strong>Status:</strong> {status}
+        </p>
+      </React.Fragment>
+    );
+  }
 };
 
 export default SubscriptionData;
