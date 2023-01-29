c0d79268577fcae8381ebee59cdba86b3c6020ad
Kinngh <kinngh@me.com>
2023-01-30 02:10:26 +0530

    🔬 Experimental Prisma update for auto complete

--- utils/prisma.js
+++ utils/prisma.js
@@ -1,11 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-let prisma;
+let prisma = new PrismaClient();
 let isProd = process.env.NODE_ENV === "production";
 
-if (isProd) {
-  prisma = new PrismaClient();
-} else {
+//EXPERIMENTAL: If it's not in production, disconnect and use global var instead.
+if (!isProd) {
+  prisma.$disconnect();
   if (!global.prisma) {
     global.prisma = new PrismaClient();
   }
