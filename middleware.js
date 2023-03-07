a7bb21a38d398c2e2b9c7c295ea729865ad59069
Kinngh <kinngh@me.com>
2023-03-08 03:34:38 +0530

    ⚠️ Fix CSP Issues

---
+++ middleware.js
@@ -0,0 +1,35 @@
+/*
+    Add Content Security Policy headers to all relevant requests.
+*/
+
+import { NextResponse } from "next/server";
+
+export const config = {
+  matcher: [
+    /*
+     * Exceptions:
+     * /api/auth, /api/webhooks, /api/proxy_route, /api/gdpr, /_next,
+     * /_proxy, /_auth, /_static, /_vercel, /public (/favicon.ico, etc)
+     */
+    "/((?!api/auth|api/webhooks|api/proxy_route|api/gdpr|_next|_proxy|_auth|_static|_vercel|[\\w-]+\\.\\w+).*)",
+  ],
+};
+
+export function middleware(request) {
+  const {
+    nextUrl: { search },
+  } = request;
+  const urlSearchParams = new URLSearchParams(search);
+  const params = Object.fromEntries(urlSearchParams.entries());
+
+  const shop = params.shop || "*.myshopify.com";
+
+  const res = NextResponse.next();
+  res.headers.set(
+    "Content-Security-Policy",
+    `frame-ancestors https://${shop} https://admin.shopify.com;`
+  );
+
+  // You can also set request headers in NextResponse.rewrite
+  return res;
+}
