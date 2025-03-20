e2c2bb439b4b89e0f1a9c09fe82cfad8117f610c
Harshdeep Singh Hura <kinngh@me.com>
2025-03-21 01:09:14 +0530

    🔨 Add dev origin

--- next.config.js
+++ next.config.js
@@ -9,6 +9,7 @@ console.log(`--> Running in ${process.env.NODE_ENV} mode`);
 
 const nextConfig = {
   reactStrictMode: true,
+  allowedDevOrigins: [process.env.SHOPIFY_APP_URL.toString().replace("https://", ""),],
   env: {
     CONFIG_SHOPIFY_API_KEY: process.env.SHOPIFY_API_KEY,
     CONFIG_SHOPIFY_APP_URL: process.env.SHOPIFY_APP_URL,
