b4e774e1669bf727e44cda7047312e9750086129
Harshdeep <kinngh@me.com>
2025-04-30 18:21:14 +0530

    🧹 Update API version in env

--- next.config.js
+++ next.config.js
@@ -8,6 +8,7 @@ console.log(`--> Running in ${process.env.NODE_ENV} mode`);
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  devIndicators: false,
   env: {
     CONFIG_SHOPIFY_API_KEY: process.env.SHOPIFY_API_KEY,
     CONFIG_SHOPIFY_APP_URL: process.env.SHOPIFY_APP_URL,
