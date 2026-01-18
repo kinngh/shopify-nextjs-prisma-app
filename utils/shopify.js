a0feb66f787544fa78a7f61718277e73a8f2e8f1
Harshdeep Singh Hura <kinngh@me.com>
2026-01-18 12:08:24 -0500

    🧹 Cleanup shopify.js

--- utils/shopify.js
+++ utils/shopify.js
@@ -40,40 +40,7 @@ shopify = {
     /**
      * @type {import("@/_developer/types/declarative.js").DeclarativeMetafieldDefinition[]}
      */
-    metafields: [
-      // {
-      //   owner_type: "product",
-      //   key: "key_name",
-      //   name: "Last synced",
-      //   description: "This is a description",
-      //   type: "number_integer",
-      //   access: {
-      //     admin: "merchant_read_write",
-      //     customer_account: "read_write",
-      //   },
-      //   capabilities: {
-      //     admin_filterable: true,
-      //     unique_values: false,
-      //   },
-      //   validations: [
-      //     {
-      //       //Docs: https://shopify.dev/docs/apps/build/metafields/list-of-validation-options
-      //       name: "max",
-      //       value: "10",
-      //     },
-      //     {
-      //       name: "min",
-      //       value: "1",
-      //     },
-      //   ],
-      // },
-    ],
-    /**
-     * @type {import("@/_developer/types/declarative.js").DeclarativeMetaobjectDefinition[]}
-     */
-    metaobjects: [
-      //Support is coming soon.
-    ],
+    metafields: [],
   },
 };
 
